c25c489a1801b31de16ac8b544a80f381bb1448e
Jacob Frideres <jakefrideres@gmail.com>
2021-10-29 18:27:39 -0500

    [Finishes #180117942] Validates the technology stack.

--- index.js
+++ index.js
@@ -20,8 +20,14 @@ express()
     try {
       const client = await pool.connect();
       
+      const tasks = await client.query(
+`SELECT * FROM tasks ORDER BY id ASC`);
+
+      const locals = {
+        'tasks': (tasks) ? tasks.rows : null
+      };
+      res.render('poges/index', locals);
       client.release();
-      res.send("Works");
     }
     catch (err) {
       console.error(err);
@@ -34,38 +40,6 @@ express()
       const tables = await client.query(
       
 `     
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  email text NOT NULL,
-  password text NOT NULL
-);
-
-CREATE TABLE students (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  school INT NOT NULL,
-  expires DATE NOT NULL
-);
-
-CREATE TABLE schools (
-  id SERIAL PRIMARY KEY, 
-  name TEXT NOT NULL,
-  address TEXT NOT NULL
-);
-
-CREATE TABLE observations (
-  id SERIAL PRIMARY KEY,
-  users_id INT NOT NULL,
-  students_id INT NOT NULL,
-  tasks_id INT NOT NULL,
-  duration INTERVAL NOT NULL
-);
-
-CREATE TABLE tasks (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL
-);
-    
 SELECT c.relname AS table, a.attname AS column, t.typname AS type
 FROM pg_catalog.pg_class AS c
 LEFT JOIN pg_catalog.pg_attribute AS a
@@ -75,13 +49,44 @@ ON a.atttypid = t.oid
 WHERE c.relname IN ('users', 'observations', 'students', 'schools', 'tasks')
 ORDER BY c.relname, a.attnum;
 `);    
+
+      const obs = await client.query(
+`SELECT * FROM observations`);
     
       const locals = {
         'tables': (tables) ? tables.rows : null
+        'obs': (obs) ? obs.rows : null
       };
       
       res.render('pages/db-info', locals);
       client.release();
+    }
+    catch (err) {
+      console.error(err);
+      res.send("Error: " + err);
+    }
+  })
+  .post('/log', async(req, res) => {
+    try {
+      const client = await pool.connect();
+      const usersId = req.body.users_id;
+      const studentsId = req.body.students_id;
+      const tasksId = req.body.tasks_id;
+      const duration = req.body.duration;
+      const sqlInsert = await client.query(
+`INSERT INTO observations (users_id, students_id, tasks_id, duration)
+VALUES (${usersId}, ${studentsId}, ${tasksId}, ${duration})
+RETURNING id as new_id;`);
+       console.log(`Tracking task ${tasksId}`);
+       
+       const result = {
+         'response: (sqlInsert) ? (sqlInsert.rows[0]) : null
+       };
+       res.set({
+         'Content-Type': 'application/json'
+       });
+       res.json({ requestBody: result });
+       client.release();
     }
     catch (err) {
       console.error(err);
